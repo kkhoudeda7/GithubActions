97ebf8696d2ecef6abe938a05a83cb7ec2f082b2
kkhoudeda7 <167578059+kkhoudeda7@users.noreply.github.com>
2024-11-08 13:32:32 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using NUnit.Framework;
 
 namespace GithubActionsLab
@@ -31,5 +31,77 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidKhoudeda()
+        {
+            Assert.AreEqual(3, Program.Subtract("5", "2"));
+            Assert.AreEqual(3, Program.Subtract("6", "3"));
+            Assert.AreEqual(-7, Program.Subtract("4", "3"));
+        }
+
+        [Test]
+        public void Subtract_InvalidKhoudeda()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_NullKhoudeda()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_ValidKhoudeda()
+        {
+            Assert.AreEqual(6, Program.Multiply("2", "3"));
+            Assert.AreEqual(18, Program.Multiply("6", "3"));
+            Assert.AreEqual(0, Program.Multiply("0", "4"));
+        }
+
+        [Test]
+        public void Multiply_InvalidKhoudeda()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_NullKhoudeda()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_ValidKhoudeda()
+        {
+            Assert.AreEqual(5, Program.Divide("25", "5"));
+            Assert.AreEqual(7, Program.Divide("21", "3"));
+            Assert.AreEqual(0, Program.Divide("0", "4"));
+        }
+
+        [Test]
+        public void Divide_InvalidKhoudeda()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_NullKhoudeda()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
     }
 }
